d43bc29119cac1f597d7e4099aeb2bc46d0a23b2
DEV-SIU <dtmartins102@gmail.com>
2026-05-26 20:57:16 -0300

    Fix: correção de paths.

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ export default function App() {
   return (
     <main className="page">
       <img
-        src="/logo.svg"
+        src={`${import.meta.env.BASE_URL}logo.svg`}
         alt="ResgatoCão"
         className="brand-logo"
         draggable={false}
